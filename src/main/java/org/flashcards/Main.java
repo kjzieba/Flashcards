74774d4e45b647ee3ab26d9d01bfc7e701515c26
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2022-12-25 22:44:37 +0100

    added get started page

--- src/main/java/org/flashcards/Main.java
+++ src/main/java/org/flashcards/Main.java
@@ -1,7 +1,9 @@
 package org.flashcards;
 
+import org.flashcards.Frontend.GetStarted;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new GetStarted();
     }
 }
