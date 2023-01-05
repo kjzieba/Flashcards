f61de0b1c80aa18c3d32fa5ac28f10c24e09022c
DanielZ707 <daniel.z.7000000@gmail.com>
2023-01-06 00:14:11 +0100

    add body of action method to flashcards

--- src/main/java/org/flashcards/src/FlashcardWithImage.java
+++ src/main/java/org/flashcards/src/FlashcardWithImage.java
@@ -32,6 +32,6 @@ public class FlashcardWithImage extends Flashcard {
 
     @Override
     public void action() {
-
+        this.flashcardState.action();
     }
 }
--- src/main/java/org/flashcards/src/FlashcardWithText.java
+++ src/main/java/org/flashcards/src/FlashcardWithText.java
@@ -22,6 +22,6 @@ public class FlashcardWithText extends Flashcard {
 
     @Override
     public void action() {
-
+        this.flashcardState.action();
     }
 }
