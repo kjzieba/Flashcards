6c9de739c3b5b42d48ef99e6532edc4e6c9dbfc9
DanielZ707 <daniel.z.7000000@gmail.com>
2023-01-09 22:04:26 +0100

    refactor edit

--- src/main/java/org/flashcards/gui/Edit.java
+++ src/main/java/org/flashcards/gui/Edit.java
@@ -11,6 +11,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.util.ArrayList;
 
 public class Edit extends JPanel {
     private final Initializer initializer;
@@ -18,6 +19,10 @@ public class Edit extends JPanel {
     private final JTextField nameTextField = new JTextField("Enter a title");
 
     JScrollPane scrollPane = new JScrollPane();
+
+    private boolean titleSet = false;
+
+    private ArrayList<Long> idCards = new ArrayList<>();
     private JPanel content = new JPanel(new GridLayout(0, 2));
 
     public Edit(Initializer initializer) {
@@ -37,7 +42,13 @@ public class Edit extends JPanel {
         backButton.addActionListener(e -> {
             initializer.update(GUInitializer.Panel.Add);
             App.getInstance().deleteRepo();
+            content.removeAll();
+            content.repaint();
+            content.revalidate();
+            scrollPane.repaint();
+            scrollPane.revalidate();
         });
+
         add(backButton);
     }
 
@@ -46,6 +57,8 @@ public class Edit extends JPanel {
         addButton.addActionListener(e -> {
             TxtCard txtCard = App.getInstance().createEmptyTxtCard();
             content.add(getTermTextArea(txtCard));
+            content.add(getDefinitionTextArea(txtCard));
+
         });
         add(addButton);
     }
@@ -56,12 +69,13 @@ public class Edit extends JPanel {
             initializer.update(GUInitializer.Panel.ChooseMode);
             nameTextField.setText("Enter a title");
             System.out.println(App.getInstance().getAllCards());
-            App.getInstance().setIdRepo(App.getInstance().getIdRepo()+1);
+            App.getInstance().setIdRepo(App.getInstance().getIdRepo() + 1);
             content.removeAll();
             content.repaint();
             content.revalidate();
             scrollPane.repaint();
             scrollPane.revalidate();
+            titleSet = false;
 
         });
         add(saveButton);
@@ -76,13 +90,20 @@ public class Edit extends JPanel {
         nameTextField.setHorizontalAlignment(JTextField.CENTER);
         nameTextField.addFocusListener(new FocusListener() {
             @Override
-            public void focusGained(FocusEvent e) {}
+            public void focusGained(FocusEvent e) {
+                if (titleSet) {
+
+                } else {
+                    nameTextField.setText("");
+                }
+            }
 
             @Override
             public void focusLost(FocusEvent e) {
                 FlashcardCollectionInterface cardsRepo = App.getInstance().getAllCards().query(App.getInstance().getIdRepo());
                 TxtFlashcardCollection txtCardRepo = (TxtFlashcardCollection) cardsRepo;
                 txtCardRepo.setTitle(nameTextField.getText());
+                titleSet = true;
             }
         });
         add(nameTextField);
@@ -100,12 +121,23 @@ public class Edit extends JPanel {
                 BorderFactory.createEmptyBorder(10, 3, 10, 0)));
         termTextArea.addFocusListener(new FocusListener() {
             @Override
-            public void focusGained(FocusEvent e) {}
+            public void focusGained(FocusEvent e) {
+                if (idCards.contains(card.getId())) {
+
+                } else {
+                    termTextArea.setText("");
+                }
+            }
 
             @Override
             public void focusLost(FocusEvent e) {
-                card.setTextQuestion(termTextArea.getText());
-                content.add(getDefinitionTextArea(card));
+                if (idCards.contains(card.getId())) {
+                    card.setTextQuestion(termTextArea.getText());
+                } else {
+                    card.setTextQuestion(termTextArea.getText());
+                    idCards.add(card.getId());
+                    App.getInstance().addTxtCard(card);
+                }
             }
         });
 
@@ -125,12 +157,22 @@ public class Edit extends JPanel {
         definitionTextArea.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
+                if (idCards.contains(card.getId())) {
+
+                } else {
+                    definitionTextArea.setText("");
+                }
             }
 
             @Override
             public void focusLost(FocusEvent e) {
-                card.setAnswer(definitionTextArea.getText());
-                App.getInstance().addTxtCard(card);
+                if (idCards.contains(card.getId())) {
+                    card.setAnswer(definitionTextArea.getText());
+                } else {
+                    card.setAnswer(definitionTextArea.getText());
+                    idCards.add(card.getId());
+                    App.getInstance().addTxtCard(card);
+                }
             }
         });
         return add(definitionTextArea);
@@ -141,8 +183,9 @@ public class Edit extends JPanel {
         scrollPane.setBounds(193, 176, 570, 350);
         scrollPane.setAutoscrolls(true);
         scrollPane.createVerticalScrollBar();
-        content.add(getTermTextArea(App.getInstance().createEmptyTxtCard()));
-        //content.add(getDefinitionTextArea(App.getInstance().createEmptyTxtCard()));
+        TxtCard txtCard = App.getInstance().createEmptyTxtCard();
+        content.add(getTermTextArea(txtCard));
+        content.add(getDefinitionTextArea(txtCard));
         scrollPane.setViewportView(content);
         add(scrollPane);
     }
